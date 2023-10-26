ad8883a5fe1be57ee6445749902df9f9e5306f3f
Quinton Coley <49097168+qcoley@users.noreply.github.com>
2023-10-26 10:52:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ db = SQLAlchemy(app)
 
 # ----------------------------------------------------------------------------------------------------------------------
 class Customer(db.Model):
+    __tablename__ = 'customers'
+    
     id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(Geometry(geometry_type='POINT'))
+    geolocation = db.Column(Geometry('POINT'))
     name = db.Column(db.String(80), unique=True)
     address = db.Column(db.String(120), unique=True)
     account_number = db.Column(db.Integer, unique=True)
@@ -25,10 +27,10 @@ class Customer(db.Model):
     area = db.Column(db.String(80), unique=True)
     job_set = db.Column(db.String(120), unique=True)
 
-    def __init__(self, location, name, address, account_number, premise_number, component_id, component_type, number_accounted,
+    def __init__(self, geolocation, name, address, account_number, premise_number, component_id, component_type, number_accounted,
                  number_off, area, job_set):
 
-        self.location = location
+        self.geolocation = geolocation
         self.name = name
         self.address = address
         self.account_number = account_number
@@ -41,72 +43,17 @@ class Customer(db.Model):
         self.job_set = job_set
 
     def __repr__(self):
-        return '<Customer %r>' % self.name
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-def get_pos(lat, lng):
-    return lat, lng
-
-
-def generate_shape_map(edit_name, edit_address):
-    sql = db.session.query(Customer)
-    gdf = gpd.GeoDataFrame.from_postgis(str(sql.statement), sql.session.bind, geom_col='location')
-
-    '''
-    if edit_name is not None:
-        print(edit_name)
-
-    if edit_address is not None:
-        print(edit_address)
-    '''
-
-    geojson = gdf.to_crs(epsg='4326').to_json()
-
-    m = folium.Map(location=[gdf.iloc[0].geometry.centroid.y, gdf.iloc[0].geometry.centroid.x], zoom_start=10)
-
-    # Create a folium choropleth object and specify the geo_data, data, columns, key_on, and other arguments
-    folium.Choropleth(geo_data=geojson, name='choropleth', data=gdf, columns=['Customer_N', 'Service_Ad'],
-                      fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2, legend_name='Customers').add_to(m)
-
-    # Create a folium geojson object from the same geo_data as the choropleth object
-    geo = folium.GeoJson(data=gdf, popup=folium.GeoJsonPopup(fields=['Customer_N', 'Service_Ad', "Link"], labels=True))
-
-    # Add the geojson object to the map using the add_to method
-    geo.add_to(m)
-
-    # Loop through the geojson features and add a reference link to each country
-    index = 0
-    for feature in geo.data["features"]:
-        customer_list = [index,
-                         feature["properties"]["Customer_N"],
-                         feature["properties"]["Service_Ad"],
-                         feature["properties"]["Account_Nu"],
-                         feature["properties"]["Premise_Nu"]]
-        # Get the country name
-        # Generate a reference link using Wikipedia
-        link = f"<a href='{url_for('info', customer=customer_list)}'>Update Record</a>"
-        # Add the link as a new property
-        feature["properties"]["Link"] = link
-        index += 1
-
-    # Save or display the map
-    m.save("templates/map.html")
-
-
-def generate_records():
-    sql = db.session.query(Customer)
-    gdf = gpd.GeoDataFrame.from_postgis(str(sql.statement), sql.session.bind, geom_col='location')
-    gdf.to_html("templates/record.html")
+        return f'<Customer {self.name}>'
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 @app.route('/')
 def root():
 
-    '''
+    
     db.create_all()
 
+    '''
     # insert a new record
     new_customer = Customer(location='POINT(-85.34 33.64)', name="customer", address="address", account_number=1, premise_number=1, component_id="",
                             component_type="", number_accounted=1, number_off=1, area="", job_set="")
@@ -123,14 +70,12 @@ def root():
 
 @app.route('/map_page')
 def map_page():
-    generate_shape_map(None, None)
-    return render_template('map_page.html', data='map.html')
+    return render_template('map_page.html')
 
 
 @app.route('/record_page')
 def record_page():
-    generate_records()
-    return render_template('record_page.html', data='record.html')
+    return render_template('record_page.html')
 
 
 @app.route('/upload_page', methods=['POST'])
@@ -156,3 +101,21 @@ def edit_page():
     # generate_shape_map('billing.shp', new_name, new_address)
 
     return render_template('mapped.html', data='map.html')
+
+
+# Create a route that returns the locations as JSON
+@app.route('/api/locations')
+def get_locations():
+    # Query the database
+    locations = db.session.query(Location).all()
+    # Convert the results to a list of dictionaries
+    data = []
+    for location in locations:
+        data.append({
+            'id': location.id,
+            'name': location.name,
+            'latitude': location.geolocation.y,
+            'longitude': location.geolocation.x
+        })
+    # Return the JSON response
+    return jsonify(data)

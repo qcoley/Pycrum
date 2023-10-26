262eb13fb3e9d17ffc1cdeb202120ed867f74c72
Quinton Coley <49097168+qcoley@users.noreply.github.com>
2023-10-26 15:49:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import geopandas as gpd
 import folium
+import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 from geoalchemy2 import Geometry
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pycrum_user:pOCwAVVMw3YDbjPfMKkHSyZpK9JGicR3@dpg-ckrvo87d47qs73f05310-a/pycrum'
-db = SQLAlchemy(app)
+con = psycopg2.connect(database="pycrum", user="pycrum_user", password="pOCwAVVMw3YDbjPfMKkHSyZpK9JGicR3", host="dpg-ckrvo87d47qs73f05310-a.ohio-postgres.render.com")
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -46,6 +44,48 @@ class Customer(db.Model):
         return f'<Customer {self.name}>'
 
 
+def generate_shape_map(shape_file, edit_name, edit_address):
+    gdf = gpd.GeoDataFrame.from_postgis("select * from customers", con, geom_col='geom' )
+
+    if edit_name is not None:
+        print(edit_name)
+
+    if edit_address is not None:
+        print(edit_address)
+
+    geojson = gdf.to_crs(epsg='4326').to_json()
+
+    m = folium.Map(location=[gdf.iloc[0].geometry.centroid.y, gdf.iloc[0].geometry.centroid.x], zoom_start=10)
+
+    # Create a folium choropleth object and specify the geo_data, data, columns, key_on, and other arguments
+    folium.Choropleth(geo_data=geojson, name='choropleth', data=gdf, columns=['Customer_N', 'Service_Ad'],
+                      fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2, legend_name='Customers').add_to(m)
+
+    # Create a folium geojson object from the same geo_data as the choropleth object
+    geo = folium.GeoJson(data=gdf, popup=folium.GeoJsonPopup(fields=['Customer_N', 'Service_Ad', "Link"], labels=True))
+
+    # Add the geojson object to the map using the add_to method
+    geo.add_to(m)
+
+    # Loop through the geojson features and add a reference link to each country
+    index = 0
+    for feature in geo.data["features"]:
+        customer_list = [index,
+                         feature["properties"]["Customer_N"],
+                         feature["properties"]["Service_Ad"],
+                         feature["properties"]["Account_Nu"],
+                         feature["properties"]["Premise_Nu"]]
+        # Get the country name
+        # Generate a reference link using Wikipedia
+        link = f"<a href='{url_for('info', customer=customer_list)}'>Update Record</a>"
+        # Add the link as a new property
+        feature["properties"]["Link"] = link
+        index += 1
+
+    # Save or display the map
+    m.save("templates/map.html")
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 @app.route('/')
 def root():
@@ -69,7 +109,7 @@ def root():
 
 @app.route('/map_page')
 def map_page():
-    return render_template('map_page.html')
+    return render_template('map_page.html', data='map.html')
 
 
 @app.route('/record_page')
@@ -100,21 +140,4 @@ def edit_page():
     # generate_shape_map('billing.shp', new_name, new_address)
 
     return render_template('mapped.html', data='map.html')
-
-
-# Create a route that returns the locations as JSON
-@app.route('/api/locations')
-def get_locations():
-    # Query the database
-    locations = db.session.query(Customer).all()
-    # Convert the results to a list of dictionaries
-    data = []
-    for location in locations:
-        data.append({
-            'id': location.id,
-            'name': location.name,
-            'latitude': location.geolocation.y,
-            'longitude': location.geolocation.x
-        })
-    # Return the JSON response
-    return jsonify(data)
+    

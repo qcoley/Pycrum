9e0bd8d6f5e125ecb2577b59d8264a6b8ec20c4c
qcoley <qcoley@uab.edu>
2023-10-30 12:03:17 -0500

    various updates.

--- app.py
+++ app.py
@@ -51,38 +51,38 @@ class Customer(db.Model):
 
 # ----------------------------------------------------------------------------------------------------------------------
 def generate_shape_map():
+    # use sql and database connection to query customer data and create a geo data frame
     gdf = gpd.GeoDataFrame.from_postgis("select * from customers", db.engine, geom_col='geolocation')
+    # set location and convert to geojson for choropleth layer
     gdf = gdf.set_crs("EPSG:4326")
     geojson = gdf.to_crs(epsg='4326').to_json()
 
     m = folium.Map(location=[gdf.iloc[0].geolocation.centroid.y, gdf.iloc[0].geolocation.centroid.x], zoom_start=8)
 
-    # Create a folium choropleth object and specify the geo_data, data, columns, key_on, and other arguments
+    # create a folium choropleth object that shows markers for each billing customer
     folium.Choropleth(geo_data=geojson, name='choropleth', data=gdf, columns=['name', 'address'],
                       fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2, legend_name='Customers').add_to(m)
 
-    # Create a folium geojson object from the same geo_data as the choropleth object
+    # create a folium geojson object from same dataset
     geo = folium.GeoJson(data=gdf, popup=folium.GeoJsonPopup(fields=['name', 'address', "link"], labels=True))
 
-    # Add the geojson object to the map using the add_to method
     geo.add_to(m)
 
-    # Loop through the geojson features and add a reference link to each country
-    index = 0
+    # loop through geojson features
     for feature in geo.data["features"]:
-        customer_list = [index,
+        # create list of customer info to pass to link
+        customer_list = [feature["properties"]["id"],
                          feature["properties"]["name"],
                          feature["properties"]["address"],
                          feature["properties"]["account_number"],
                          feature["properties"]["premise_number"]]
-        # Get the country name
-        # Generate a reference link using Wikipedia
-        link = f"<a href='{url_for('info_page', customer=customer_list)}'>Update Record</a>"
-        # Add the link as a new property
-        feature["properties"]["link"] = link
-        index += 1
 
-    # Save or display the map
+        # generate a reference link to customer
+        link = f"<a href='{url_for('info_page', customer=customer_list)}'>Update Record</a>"
+        # add link as new field to generate in map popup
+        feature["properties"]["link"] = link
+
+    # save map to be displayed
     m.save("templates/map.html")
 
 
@@ -92,6 +92,7 @@ def root():
     return render_template('home_page.html')
 
 
+# map page that show the map rendered with map generation
 @app.route('/map_page')
 def map_page():
     return render_template('map_page.html')
@@ -104,12 +105,14 @@ def map_view():
     return render_template('map.html')
 
 
+# record page that shows all customer information from a database query
 @app.route('/record_page')
 def record_page():
     data = db.session.execute(text("SELECT * FROM customers"))
     return render_template('record_page.html', data=data)
 
 
+# for processing uploaded shape files
 @app.route('/upload_page', methods=['POST'])
 def upload_page():
     # file = request.files.get('file')
@@ -119,27 +122,20 @@ def upload_page():
     return redirect('/')
 
 
+# shows more information for customer when clicked on from map link
 @app.route('/info_page')
 def info_page():
     customer = request.args.getlist('customer')
     return render_template('info_page.html', data=customer)
 
 
-@app.route('/edit_page', methods=['POST'])
-def edit_page():
-    new_name = request.form.get('name')
-    new_address = request.form.get('address')
-
-    # generate_shape_map('billing.shp', new_name, new_address)
-
-    return render_template('mapped.html', data='map.html')
-
-
+# add record from the records page
 @app.route('/add_record', methods=['POST'])
 def add_record():
     return render_template('add_record.html')
 
 
+# route that handles adding the customer by getting form data and inserting into database
 @app.route('/add_customer', methods=['POST'])
 def add_customer():
 
@@ -166,5 +162,19 @@ def add_customer():
     return render_template('add_record.html')
 
 
-if __name__ == "__main__":
-    app.run()
+# route that handles adding the customer by getting form data and inserting into database
+@app.route('/update_customer', methods=['POST'])
+def update_customer():
+
+    u_id = request.form.get("update_id")
+    u_name = request.form.get("update_name")
+    u_address = request.form.get('update_address')
+    u_account = request.form.get('update_account')
+    u_premise = request.form.get('update_premise')
+
+    print(u_id, u_name, u_address, u_account, u_premise)
+
+    generate_shape_map()
+    return render_template('map.html')
+
+
